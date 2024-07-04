862cd14d8819be55cbb8f0d387f31439207d12f2
김진한 <kimjh0417@gsitm.com>
2024-07-04 16:58:06 +0900

    ADD 첫시작 이미지

---
+++ lib/pages/splash_page.dart
@@ -0,0 +1,32 @@
+import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
+
+class SplashPage extends StatefulWidget {
+  const SplashPage({super.key});
+
+  @override
+  _SplashPageState createState() => _SplashPageState();
+}
+
+class _SplashPageState extends State<SplashPage> {
+  @override
+  void initState() {
+    super.initState();
+    _navigateToHome();
+  }
+
+  _navigateToHome() async {
+    await Future.delayed(const Duration(seconds: 3), () {});
+    context.go('/dm');
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.white,
+      body: Center(
+        child: Image.asset('images/sombti.jpg'),
+      ),
+    );
+  }
+}
--- lib/router.dart
+++ lib/router.dart
@@ -2,11 +2,19 @@ import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
 import 'package:somebti_front/pages/chat_page.dart';
 import 'package:somebti_front/pages/direct_message_page.dart';
+import 'package:somebti_front/pages/splash_page.dart';
 
 final GoRouter router = GoRouter(
+  initialLocation: '/',
   routes: <RouteBase>[
     GoRoute(
       path: '/',
+      builder: (BuildContext context, GoRouterState state) {
+        return const SplashPage();
+      },
+    ),
+    GoRoute(
+      path: '/dm',
       builder: (BuildContext context, GoRouterState state) {
         return const DirectMessagePage();
       },
